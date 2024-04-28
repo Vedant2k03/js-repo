614e0f5d14ae114c9fa52099219e377ac5b84f83
Vedant Devre <96368377+Vedant2k03@users.noreply.github.com>
2024-04-28 11:41:56 +0000

    control flow

---
+++ notes/control_flow/one.js
@@ -0,0 +1,25 @@
+// if
+const isUserloggedIn = true;
+const temperature = 41;
+
+// if ( temperature === 40 ){
+//     console.log("less than 50");
+// } else {
+//     console.log("temperature is greater than 50");
+// }
+
+// console.log("Execute");
+// <, >, <=, >=, ==, !=, ===, !==
+
+const userLoggedIn = true;
+const debitCard = true;
+const loggedInFromGoogle = false;
+const loggedInFromEmail = true;
+
+if (userLoggedIn && debitCard && 2 == 3) {
+  console.log("Allow to buy course");
+}
+
+if (loggedInFromGoogle || loggedInFromEmail) {
+  console.log("User logged in");
+}
